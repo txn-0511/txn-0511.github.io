9d01fed52978a16f26743f5409e337bd6c4c24c8
txn-0511 <tanjirou2910@gmail.com>
2024-10-13 23:15:02 +0700

    update (#1)
    Co-authored-by: Barry (Toan) <167836621+barry-toan@users.noreply.github.com>

--- _next/static/1mWAxQqlQTg0XDb2pX97U/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/125240fa0b63d02e.css","static/chunks/pages/index-6fc52e489e2a2ba8.js"],"/_error":["static/chunks/pages/_error-fdc5cf1f34cb592f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
