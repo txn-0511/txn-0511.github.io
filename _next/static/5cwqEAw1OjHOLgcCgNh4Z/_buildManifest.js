2b9a26ba30810d8a20cb5314ee80c283a9a1f7e2
Barry (Toan) <167836621+barry-toan@users.noreply.github.com>
2024-10-15 09:36:21 +0700

    update

--- _next/static/5cwqEAw1OjHOLgcCgNh4Z/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/125240fa0b63d02e.css","static/chunks/pages/index-89bf9aee539ecdb1.js"],"/_error":["static/chunks/pages/_error-fdc5cf1f34cb592f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
